ac35ca9ebadba3aef42b919558032ddade640b23
Hugo <hquezada@leomail.tamuc.edu>
2025-11-06 16:50:53 -0600

    feat: create Home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
+<!-- HOME PAGE (views/HomeView.vue) -->
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import HeroSection from '@/components/HeroSection.vue'
+import FeaturedProducts from '@/components/FeaturedProducts.vue'
+import AboutPreview from '@/components/AboutPreview.vue'
+import CTASection from '@/components/CTASection.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home-page">
+    <HeroSection />
+    <FeaturedProducts />
+    <AboutPreview />
+    <CTASection />
+  </div>
 </template>
+
+<style scoped>
+.home-page {
+  width: 100%;
+}
+</style>
